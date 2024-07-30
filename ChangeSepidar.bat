76185963fcc9f4db2e62fa543941f667b1c1e1c7
Mohammad <ma7@email.com>
2024-07-30 18:39:45 +0330

    Basic Changes
    1403 / 05 / 09

--- ChangeSepidar.bat
+++ ChangeSepidar.bat
@@ -4,26 +4,30 @@ echo  --------------------
 echo.
 echo  Chose a System:
 echo.
-echo  [1] TOP
-echo  [2] Tempo
-echo  [3] Wistore
+echo  [1] Tempo (Poiandegan)
+echo  [2] Wistore (Shokouh)
 echo.
 set /p number=
 
 if %number% == 1 (
-    netsh wlan connect name="Accounting"
-    del "C:\Sepidar\user.config"
-    copy "C:\MEGA\Code\Administration\Change Sepidar Connection\Files\Top Client\user.config" "C:\Sepidar\user.config" /Y
+    del "C:\App\Sepidar\user.config"
+    del "C:\App\Sepidar\0057230 (1).License"
+    del "C:\App\Sepidar\0063452 (2).License"
+    del "C:\App\Sepidar\VersionLicense.cdx"
+    copy "C:\App\Change Sepidar Connection\Files\Tempo Client\user.config" "C:\App\Sepidar\user.config" /Y
+    copy "C:\App\Change Sepidar Connection\Files\Tempo Client\0057230 (1).License" "C:\App\Sepidar\0057230 (1).License" /Y
+    copy "C:\App\Change Sepidar Connection\Files\Tempo Client\VersionLicense.cdx" "C:\App\Sepidar\VersionLicense.cdx" /Y
 ) else if %number% == 2 (
-    netsh wlan connect name="wistore"
-    copy "C:\MEGA\Code\Administration\Change Sepidar Connection\Files\Tempo Client\user.config" "C:\Sepidar\user.config" /Y
-) else if %number% == 3 (
-    netsh wlan connect name="wistore"
-    copy "C:\MEGA\Code\Administration\Change Sepidar Connection\Files\Wistore Client\user.config" "C:\Sepidar\user.config" /Y
+    del "C:\App\Sepidar\user.config"
+    del "C:\App\Sepidar\0057230 (1).License"
+    del "C:\App\Sepidar\0063452 (2).License"
+    del "C:\App\Sepidar\VersionLicense.cdx"
+    copy "C:\App\Change Sepidar Connection\Files\Wistore Client\user.config" "C:\App\Sepidar\user.config" /Y
+    copy "C:\App\Change Sepidar Connection\Files\Wistore Client\0063452 (2).License" "C:\App\Sepidar\0063452 (2).License" /Y
+    copy "C:\App\Change Sepidar Connection\Files\Wistore Client\VersionLicense.cdx" "C:\App\Sepidar\VersionLicense.cdx" /Y
 ) else (
-    echo The number is not 1, 2, or 3.
+    echo The number is not 1 or 2.
+    pause
 )
 
-start C:\Sepidar\Shell.exe
-
-pause
+start C:\App\Sepidar\Shell.exe
